56215879641fbb7de35a64a33e62de2d28655497
Anant Sharma <anantatnet@gmail.com>
2020-07-13 13:05:49 +0530

    Added Constructor Overloading

---
+++ CSharpFundamentals/Classes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CSharpFundamentals
+{
+    public class Classes
+    {
+        public int Id;
+        public string Name;
+
+        //Default Constructor
+        public Classes()
+        {
+        }
+
+        // Overload Constructor
+        public Classes(int id)
+        {
+            this.Id = id;
+        }
+
+        // Overload Constructor with exchange control flow
+        // this(id) will call `public Classes(int id)`
+        public Classes(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
