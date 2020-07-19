4f6f55fb0a4e8955bef9bb5abb8dfa6d3991666e
Anant Sharma <anantatnet@gmail.com>
2020-07-20 00:54:07 +0530

    Added Linq and Collection Extension

---
+++ CollectionExtensions/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Create List
+            List<Customer> customers = new List<Customer>();
+
+            customers.Add(new Customer
+            {
+                ID = "1",
+                Name = "C1",
+                Salary = 5000,
+            });
+
+            customers.Add(new Customer
+            {
+                ID = "2",
+                Name = "C2",
+                Salary = 15000,
+            });
+
+            customers.Add(new Customer
+            {
+                ID = "3",
+                Name = "C3",
+                Salary = 2000,
+            });
+
+            // Iterate List
+            foreach (Customer c in customers)
+            {
+                Console.WriteLine(c.Format());
+            }
+
+            // Iterate List
+            customers.ForEach(c => {
+                Console.WriteLine(c.Format());
+            });
+
+            // Create Dictionary
+            Dictionary<string, Customer> customerIndex = new Dictionary<string, Customer>();
+            foreach (Customer c in customers)
+            {
+                customerIndex.Add(c.ID, c);
+            }
+
+            Console.WriteLine(customerIndex["1"].Salary);
+
+            // Key Value pair
+            foreach(KeyValuePair<string, Customer> ckv in customerIndex)
+            {
+                Console.WriteLine(string.Format("Key: {0}, Value.Salary: {1}", ckv.Key, ckv.Value.Salary));
+            }
+        }
+    }
+
+    class Customer
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public string Format()
+        {
+            return string.Format("ID: {0} Name: {1} Salary {2}", this.ID, this.Name, this.Salary);
+        }
+    }
+}
---
+++ LinqFundamentals/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            UsingArray();
+            UsingList();
+        }
+
+        static void UsingArray()
+        {
+            Person[] persons = new Person[2];
+
+            // Add Person 1
+            persons[0] = new Person
+            {
+                ID = 1,
+                Name = "Person 1",
+                Salary = 2000,
+            };
+
+            // Add Person 2
+            persons[1] = new Person
+            {
+                ID = 2,
+                Name = "Person 2",
+                Salary = 5000,
+            };
+
+            // Query
+            var p_gte_2500 = from p in persons where p.Salary > 2500 select p;
+
+            foreach (var p in p_gte_2500)
+            {
+                Console.WriteLine(p.Format());
+            }
+
+        }
+
+        static void UsingList()
+        {
+            List<Person> persons = new List<Person>();
+
+            // Add Person 1
+            persons.Add(new Person
+            {
+                ID = 1,
+                Name = "Person 1",
+                Salary = 2000,
+            });
+
+            // Add Person 2
+            persons.Add(new Person
+            {
+                ID = 2,
+                Name = "Person 2",
+                Salary = 5000,
+            });
+
+            // Query
+            var p_lte_2500 = from p in persons where p.Salary < 2500 select p;
+
+            foreach (var p in p_lte_2500)
+            {
+                Console.WriteLine(p.Format());
+            }
+        }
+    }
+
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public string Format()
+        {
+            return string.Format("ID: {0} Name: {1} Salary {2}", this.ID, this.Name, this.Salary);
+        }
+    }
+}
